2656138cbe036e2f3092515616a3bc0d9cb5678e
Ian Mihura <mihura.ian@gmail.com>
2018-05-16 20:10:52 +0200

    add goethe, return home

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,5 @@
-var isBible = false;
+var isBible = true;
+var isGoethe = true;
 
 var thenew = {
     "1": "It ",
@@ -18,17 +19,27 @@ var thenew = {
     "15": "— enlarged"
 }
 
+function goethetoggle() {
+    if (isGoethe){
+        document.getElementById('byian').classList.remove("invisible");
+        document.getElementById('bygoethe').classList.add("invisible");    
+    } else {
+        document.getElementById('byian').classList.add("invisible");
+        document.getElementById('bygoethe').classList.remove("invisible");    
+    }
+    
+    isGoethe = !isGoethe;
+}
+
 function bibleToggle() {
     for (var i in thenew) {
         var sp = document.getElementById(i);
 
-        if (!isBible)
+        if (isBible)
             sp.innerText = thenew[i];
         else
             sp.innerText = "";
     }
 
     isBible = !isBible;
-    console.log(a);
-    // TODO execute toggle bible
 }
