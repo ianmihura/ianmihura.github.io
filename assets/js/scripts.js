375dd59899a3f260e6d0f672ba23f1c5df8dad00
Ian Mihura <mihura.ian@gmail.com>
2019-10-19 16:58:53 +0200

    always open menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,11 @@
 // dl-menu options
 $(function() {
-  $( '#dl-menu' ).dlmenu({
-    animationClasses : { classin : 'dl-animate-in', classout : 'dl-animate-out' }
-  });
+    $('#dl-menu').dlmenu({
+        animationClasses: {
+            classin: 'dl-animate-in',
+            classout: 'dl-animate-out'
+        }
+    });
 });
 // Need this to show animation when go back in browser
 window.onunload = function() {};
@@ -12,7 +15,7 @@ $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gi
 
 // FitVids options
 $(function() {
-  $(".content").fitVids();
+    $(".content").fitVids();
 });
 
 // All others
@@ -38,43 +41,56 @@ $(document).ready(function() {
         arrowColor: '#000',
         goupSpeed: 'normal'
     });
-	$('.image-popup').magnificPopup({
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-    },
-    image: {
-      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
-    },
-    removalDelay: 300, // Delay in milliseconds before popup is removed
-    // Class that is added to body when popup is open.
-    // make it unique to apply your CSS animations just to this exact popup
-    mainClass: 'mfp-fade'
-  });
+    $('.image-popup').magnificPopup({
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
+        },
+        removalDelay: 300, // Delay in milliseconds before popup is removed
+        // Class that is added to body when popup is open.
+        // make it unique to apply your CSS animations just to this exact popup
+        mainClass: 'mfp-fade'
+    });
 
-  $("p").each(function() {
-      if($(this).html()=='[expand]') {
-          $(this).addClass('expand_start');
-      }
-      if($(this).html()=='[/expand]') {
-          $(this).addClass('expand_end');
-      }
-  });
+    $("p").each(function() {
+        if ($(this).html() == '[expand]') {
+            $(this).addClass('expand_start');
+        }
+        if ($(this).html() == '[/expand]') {
+            $(this).addClass('expand_end');
+        }
+    });
 
-  $( "p.expand_start" ).each(function() {
-      $(this).nextUntil( $("p.expand_end") ).wrapAll("<div class='expand' style='overflow: hidden;'></div>");
-      $(this).remove()
-  });
+    $("p.expand_start").each(function() {
+        $(this).nextUntil($("p.expand_end")).wrapAll("<div class='expand' style='overflow: hidden;'></div>");
+        $(this).remove()
+    });
 
-  $( ".expand").hide();
-  $( "p.expand_end" ).hide();
+    $(".expand").hide();
+    $("p.expand_end").hide();
 
-  // modal
+    //isMobile RegEx
+    isMobile = function() {
+        var check = false;
+        (function(a) {
+            if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+        })(navigator.userAgent || navigator.vendor || window.opera);
+        return check;
+    };
 
-  // Get the modal
+    // menu always open on screens, normal on mobile
+    var menu = document.getElementsByClassName("dl-menu")[0];
+    if (!isMobile()) menu.className += " dl-menuopen";
+
+    // modal
+
+    // Get the modal
     var modals = document.getElementsByClassName("modal");
 
     for (var i = 0; i < modals.length; i++) {
@@ -83,7 +99,7 @@ $(document).ready(function() {
         var img = modal.parentElement.children[0].children[0];
         var modalImg = modal.children[1];
         var captionText = modal.children[1];
-        img.onclick = function(){
+        img.onclick = function() {
             modal.style.display = "block";
             modalImg.src = this.src;
             captionText.innerHTML = this.alt;
@@ -94,7 +110,7 @@ $(document).ready(function() {
 
         // When the user clicks on <span> (x), close the modal
         modal.onclick = function() {
-          modal.style.display = "none";
+            modal.style.display = "none";
         }
     }
 
