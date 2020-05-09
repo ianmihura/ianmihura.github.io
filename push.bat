1e52d443cf872d0156a746e41402f619c25d1a1d
Ian Mihura <mihura.ian@gmail.com>
2020-05-10 00:36:31 +0200

    commit

--- push.bat
+++ push.bat
@@ -1,13 +1,31 @@
+echo "$ bundle exec jekyll build"
 CALL bundle exec jekyll build
+echo
+echo "$ git add ."
 CALL git add .
+echo
+echo "$ git commit"
 CALL git commit -m "commit"
+echo
+echo "git push origin gh-pages"
 CALL git push origin gh-pages
-echo "------------------------------"
-echo "---- GH-PAGES BRANCH DONE ----"
-echo "--- MOVING TO MASTER BRANCH --"
-echo "------------------------------"
+echo 
+echo "#### gh-pages branch DONE"
+echo "#### Moving to master branch"
+echo 
 CALL cd _site
+echo "#### Currently on ./_site"
+echo
+echo "$ git add ."
 CALL git add .
+echo
+echo "$ git commit"
 CALL git commit -m "commit"
+echo
+echo "git push origin master"
 CALL git push origin master
+echo "#### master branch DONE"
+echo 
+CALL cd ..
+echo "#### Currently on ./"
 pause
