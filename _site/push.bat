364c1c0e258527f324e5b22d2b6cceea875eb135
Ian Mihura <mihura.ian@gmail.com>
2020-05-10 00:28:44 +0200

    commit

--- _site/push.bat
+++ _site/push.bat
@@ -6,4 +6,8 @@ echo "------------------------------"
 echo "---- GH-PAGES BRANCH DONE ----"
 echo "--- MOVING TO MASTER BRANCH --"
 echo "------------------------------"
+CALL cd _sites
+CALL git add .
+CALL git commit -m "commit"
+CALL git push origin master
 pause
